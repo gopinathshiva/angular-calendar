5c8c147a6917e888ad36827a351cbd700b34463b
unknown <sgopinath31@gmail.com>
2015-07-23 21:32:28 +0530

    final commit

--- app.js
+++ app.js
@@ -1,62 +1,59 @@
+/*
+ * filename: app.js
+ * author: gopi
+ * created_at: 22-7-15
+ * description: created myApp module to run Angular app, consist of myCtrl (controller) and myService (service)
+ */
+
+"use strict";
+
 var app = angular.module('myApp', []);
 
-app.controller('myCtrl', function($scope, myService) {
+app.controller('myCtrl', function ($scope, myService) {
     var eventDatas = [];
     $scope.currentEventDatas = [];
 
     $scope.eventContainers = new Array(24);
     $scope.times = myService.getTimes();
 
-    $scope.onClickToday = function() {
+    $scope.onClickToday = function () {
         if (+$scope.currentDate.setHours(0, 0, 0, 0) === +new Date().setHours(0, 0, 0, 0))
             return;
 
         $scope.currentDate = new Date();
         $scope.currentEventDatas = myService.getCurrentDayDatas($scope.currentDate, eventDatas);
     };
-    
-    $('.event-container').on('mouseover','.event-details',function(){
-        console.log('mouseover');
-    });
 
-    $scope.previousDay = function() {
+    $scope.previousDay = function () {
         $scope.currentDate.addDays(-1);
         $scope.currentEventDatas = myService.getCurrentDayDatas($scope.currentDate, eventDatas);
     };
 
-    $scope.nextDay = function() {
+    $scope.nextDay = function () {
         $scope.currentDate.addDays(1);
         $scope.currentEventDatas = myService.getCurrentDayDatas($scope.currentDate, eventDatas);
     };
 
-    $scope.onMouseOver = function() {        
-        angular.element(this).addClass('highlight-event');
-    };
-
-    $scope.onMouseLeave = function() {
-        angular.element(this).removeClass('highlight-event');
-    };
-
-    $scope.getEventTime = function(date) {
+    $scope.getEventTime = function (date) {
         if (date)
             return new Date(date);
     };
 
-    $scope.getEventDatas = function() {
+    $scope.getEventDatas = function () {
         $scope.currentDate = new Date();
-        myService.get().then(function(data) {
+        myService.get().then(function (data) {
             eventDatas = data.data;
             $scope.currentEventDatas = myService.getCurrentDayDatas(new Date(), eventDatas);
         });
     };
 });
 
-app.service('myService', function($http) {
+app.service('myService', function ($http) {
     var api = {};
-    api.get = function() {
+    api.get = function () {
         return $http.get('sample-data.json');
     };
-    api.getTimes = function() {
+    api.getTimes = function () {
         var times = [];
         for (var i = 1; i <= 23; i++) {
             if (i < 12) {
@@ -72,10 +69,10 @@ app.service('myService', function($http) {
         times.push('12 PM');
         return times;
     };
-    api.getCurrentDayDatas = function(currentDate, allEventDatas) {
+    api.getCurrentDayDatas = function (currentDate, allEventDatas) {
         console.time('exec');
         currentDate.setHours(0, 0, 0, 0);
-        var currentDayDatas = allEventDatas.filter(function(data) {
+        var currentDayDatas = allEventDatas.filter(function (data) {
             //debugger
             var date = new Date(data.startTime);
             var temp = new Date(data.startTime);
@@ -87,11 +84,11 @@ app.service('myService', function($http) {
                 data.height = endDate.getHours() * 60;
                 data.height += endDate.getMinutes();
                 data.height -= data.marginTop;
-                if(data.height<=30 && data.height>15){
+                if (data.height <= 30 && data.height > 15) {
                     data.fontSize = '11px';
-                }else if(data.height<=15){
+                } else if (data.height <= 15) {
                     data.fontSize = '9px';
-                }else{
+                } else {
                     data.fontSize = 'inherit';
                 }
                 return data;
@@ -103,7 +100,7 @@ app.service('myService', function($http) {
     return api;
 });
 
-Date.prototype.addDays = function(n) {
+Date.prototype.addDays = function (n) {
     var time = this.getTime();
     var changedDate = new Date(time + (n * 24 * 60 * 60 * 1000));
     this.setTime(changedDate.getTime());
